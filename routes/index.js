b03e29196f1d7deda779e7e8812b8e684b526607
youme <mrsimplelife@naver.com>
2021-02-20 02:25:27 +0900

    add v1 api using

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,52 @@
 const axios = require("axios");
 
 const router = require("express").Router();
+const URL = "http://localhost:3001/v1";
+axios.defaults.headers.origin = "http://localhost:4000";
+
+const request = async (req, api) => {
+  try {
+    if (!req.session.jwt) {
+      // 세션에 토큰이 없으면 토큰 발급 시도
+      const tokenResult = await axios.post(`${URL}/token`, {
+        clientSecret: process.env.CLIENT_SECRET,
+      });
+      req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
+    }
+    return await axios.get(`${URL}${api}`, {
+      headers: { authorization: req.session.jwt },
+    });
+  } catch (error) {
+    console.error(error);
+    if (error.response.status === 419) {
+      delete req.session.jwt;
+      return request(req, api);
+    }
+    return error.response;
+  }
+};
+
+router.get("/mypost", async (req, res, next) => {
+  try {
+    const result = await request(req, "/posts/my");
+    res.json(result.data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+router.get("/search/:hashtag", async (req, res, next) => {
+  try {
+    const result = await request(
+      req,
+      `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`
+    );
+    res.json(result.data);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 router.get("/test", async (req, res, next) => {
   // 토큰 테스트 라우터
